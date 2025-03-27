4db832d73362001f338ada3c3714452193d42e54
rajrupam <rajrupam000@gmail.com>
2025-03-27 17:54:42 +0530

    changes

---
+++ dist/assets/AxiosToastError-CEahqkfS.js
@@ -0,0 +1,1 @@
+import{s as t}from"./index-ChrwmaKA.js";const p=s=>{var a,o;t((o=(a=s==null?void 0:s.response)==null?void 0:a.data)==null?void 0:o.message,"error")};export{p as A};
---
+++ dist/assets/NoData-OS1XLSRu.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-ChrwmaKA.js";const s=()=>t.jsx("div",{className:"flex items-center justify-items-center",children:"No Data"});export{s as N};
